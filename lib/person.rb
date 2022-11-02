1b8dbd4a4c569087201a4bf53251f2882b216e0b
fliblip <wickynjy@gmail.com>
2022-11-02 16:16:09 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+person_attributes = {
+  name: "Bob",
+  birthday: "26th october",
+  hair_color: "Brown",
+  eye_color: "Green",
+  height: "5'11\"",
+  weight: "65kgs",
+  handed: "left",
+  complexion: "dark",
+  t_shirt_size: "L",
+  wrist_size: "4 in",
+  glove_size: "L",
+  pant_length: "10 in",
+  pant_width: "6 in"
+
+}
